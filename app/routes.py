f92de3438d171632bf27d434323aff03ca1ae4a4
Rubens Lima CPD07 <rubenslima@gmail.com>
2024-02-22 12:21:10 -0300

    atualizando rotas

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
+
 def index():
-    return "hello world!"
+    opcao ='Teste de parametro'
+    dados = {'linha01':'Primeira linha','linha02':'Segunda linha'}
+    return render_template('index.html', opcao=opcao, dados=dados)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
